88e478010d39709d4ecb8373f8a13b2fe27bbcef
Sakshi Jain <t-sakj@microsoft.com>
2020-06-28 16:18:07 +0530

    Add project files.

---
+++ cosmosdbquery/Program.cs
@@ -0,0 +1,31 @@
+﻿using cosmosdbquery;
+using Microsoft.Azure.Cosmos;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ChoETL;
+using Newtonsoft.Json;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace program
+{
+    public class mainclass
+    {
+
+        
+        public static async Task Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.Commercial;
+            process methodobject = new process();
+
+            await methodobject.Readfromdb();
+       
+        }
+    }
+}
+
+
